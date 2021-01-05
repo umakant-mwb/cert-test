9a7438bf27b76f57333e5f07c16023bd4a40a5ec
cedcoss <umakantsharma@makewebbetter.com>
2021-01-05 16:30:53 +0530

    fix failed build

--- wp-content/plugins/my-plugin/tests/acceptance/CheckActiveCept.php
+++ wp-content/plugins/my-plugin/tests/acceptance/CheckActiveCept.php
@@ -3,4 +3,4 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('check if plugin is active');
 $I->loginAsAdmin();
 $I->amOnPluginsPage();
-$I->seePluginDeactivated('my-plugin');
+$I->seePluginActivated('my-plugin');
